259cc6eaec8121801b1e5dd376afe2c4e07d06a4
Zafar4774 <70778474+Zafar4774@users.noreply.github.com>
2022-01-27 17:11:42 +0500

    Update script.py

--- script.py
+++ script.py
@@ -4,7 +4,5 @@ with open('script.txt', 'w') as f:
   for pid in psutil.pids():
     p = psutil.Process(pid=pid)
     if p.status() == 'running' and p.pid == pid:
-      s = f'{p.pid}-----------{p.name()}\n'
-      print(s)
       f.write(s)
 
